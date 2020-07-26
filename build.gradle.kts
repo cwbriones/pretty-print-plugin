da208a2b2a070780b7958a4004c8fe98f82a5ea6
Christian Briones <cwbriones@gmail.com>
2020-07-26 12:34:08 -0700

    extract PrettyPrintTestPlugin into its own project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,40 +1,33 @@
 plugins {
     java
-    application
-    id("com.github.johnrengelman.shadow") version "6.0.0"
-    id("lint")
-    id("testing")
+    `kotlin-dsl`
+    `maven-publish`
 }
 
-group = "io.briones.example"
+group = "io.briones.gradle"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
-application {
-    mainClassName = "io.briones.example.Main"
-}
-
 java {
     sourceCompatibility = JavaVersion.VERSION_11
 }
 
+kotlinDslPluginOptions {
+    experimentalWarning.set(false)
+}
+
 dependencies {
-    implementation(group = "org.slf4j", name = "slf4j-api", version = "2.0.0-alpha1")
-    runtimeOnly(group = "ch.qos.logback", name = "logback-core", version = "1.3.0-alpha5")
-    runtimeOnly(group = "ch.qos.logback", name = "logback-classic", version = "1.3.0-alpha5")
-    compileOnly(group = "org.immutables", name = "value", version = "2.8.2")
-    annotationProcessor(group = "org.immutables", name = "value", version = "2.8.2")
+    implementation(gradleApi())
 }
 
-sourceSets {
-    main {
-        java.srcDirs.add(file("./build/generated/sources"))
+gradlePlugin {
+    plugins {
+        create("prettyPrintTestPlugin") {
+            id = "io.briones.pretty-print"
+            implementationClass = "io.briones.gradle.PrettyPrintTestPlugin"
+        }
     }
-}
-
-tasks.withType<Tar>().configureEach {
-    compression = Compression.GZIP
 }
--- src/main/java/io/briones/example/package-info.java
+++
@@ -1,7 +0,0 @@
-@DefaultAnnotationForParameters(NonNull.class)
-@DefaultAnnotationForMethods(NonNull.class)
-package io.briones.example;
-
-import edu.umd.cs.findbugs.annotations.DefaultAnnotationForMethods;
-import edu.umd.cs.findbugs.annotations.DefaultAnnotationForParameters;
-import edu.umd.cs.findbugs.annotations.NonNull;
